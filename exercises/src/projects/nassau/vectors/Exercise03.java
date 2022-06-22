644dc276ad4aae65cff1d9b8dcdea0711fa47f3c
Mateus Henrique <mateushcandido5@gmail.com>
2022-06-21 22:41:07 -0300

    changes in exercise three

--- exercises/src/projects/nassau/vectors/Exercise03.java
+++ exercises/src/projects/nassau/vectors/Exercise03.java
@@ -1,10 +1,11 @@
 package projects.nassau.vectors;
 
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class Exercise03 {
     public static void main(String[] args){
+        /**/
+
         Scanner input = new Scanner(System.in);
         int [] Q = new int [5];
         int value= 0, largest = 0, position=0;
@@ -12,6 +13,13 @@ public class Exercise03 {
         for (int x = 0; x < Q.length; x++){
             System.out.print("Insert a value: ");
             value = input.nextInt();
+            if(value <0){
+                while(value <0){
+                    System.out.println("I'm sorry, but only positive values will be accepted.");
+                    System.out.print("Enter a value again: ");
+                    value = input.nextInt();
+                }
+            }
             Q[x] = value;
             if (value > largest) {
                 largest = value;
